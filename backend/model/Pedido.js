9d2dd801549dace05cb3e3c64c423376991da84e
Kmends <129119867+Kmends@users.noreply.github.com>
2023-04-24 20:12:15 -0300

    Add files via upload

---
+++ backend/model/Pedido.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const db = require("../database/db");
+
+const Pedido = db.define("pedido", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nomeCliente: {
+    type: DataTypes.STRING
+  }, 
+  nomeProduto: {
+    type: DataTypes.STRING
+  },
+  ingredientes: {
+    type: DataTypes.STRING
+  },
+  sabor: {
+    type: DataTypes.STRING
+  },
+  formaPagamento: {
+    type: DataTypes.STRING
+  },
+  enderecoCliente: {
+    type: DataTypes.STRING
+  },
+  observacoes: {
+    type: DataTypes.STRING
+  }
+},{
+  timestamps: false
+});
+
+Pedido.sync({ force: false });
+module.exports = Pedido;
